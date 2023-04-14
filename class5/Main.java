90465cd5b18b875b074ed6332754ab53f1460129
yerkortiz <yerko.ortizmora@gmail.com>
2023-04-14 09:15:04 -0400

    class 8

--- class5/Main.java
+++ class5/Main.java
@@ -1,6 +1,6 @@
 package class5;
-import java.util.ArrayList;
-import java.lang.Math;
+//import java.util.ArrayList;
+//import java.lang.Math;
 
 public class Main {
     public static void main(String[] args) {
---
+++ class7/SelectionSort.java
@@ -0,0 +1,17 @@
+package class7;
+
+public class SelectionSort {
+    void selectionSort(int arr[]) {
+        for (int i = 0; i < arr.length-1; i++) {
+            int min = i;
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[j] < arr[min]) {
+                    min = j;
+                }
+            }
+            int temp = arr[min];
+            arr[min] = arr[i];
+            arr[i] = temp;
+        }
+    }
+}
