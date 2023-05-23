f464be868ae1b9eed2a4eeb75de6f4dc10e5f88b
yerkortiz <yerko.ortizmora@gmail.com>
2023-05-23 09:21:03 -0400

    class 13

---
+++ class13/Main.java
@@ -0,0 +1,17 @@
+package class13;
+
+public class Main {
+    public static void main(String[] args) {
+        BST tree = new BST();
+        tree.insert(5);
+        tree.insert(2);
+        tree.insert(6);
+        //System.out.println(tree.search(5));
+        //System.out.println(tree.search(6));
+
+        //BST.inOrder(tree.root);
+        //BST.postOrder(tree.root);
+        //BST.preOrder(tree.root);
+        //tree.levelOrder();
+    }
+}
